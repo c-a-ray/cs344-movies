5570186b3ceade0eca5257e13f1f8d6848a646d9
codyray0 <c.a.ray@outlook.com>
2021-10-06 14:08:40 -0700

    Start parsing command line arguments

--- movies.c
+++ movies.c
@@ -14,19 +14,49 @@ struct Movie
     struct Movie *next;
 };
 
+char *getFilepath(int, char **);
 struct Movie *processFile(char *, int *);
 struct Movie *createMovie(char *);
 void parseLanguages(struct Movie *, char *);
 void printMovie(struct Movie *);
 void printMovieList(struct Movie *, int);
+int getQueryOption(void);
+void printOptions(void);
+void printMoviesWithYear(struct Movie *, int);
 
-int main()
+int main(int argc, char *argv[])
 {
-    char *filepath = "data/movies_sample_1.csv";
+    char *filepath = getFilepath(argc, argv);
+    if (strlen(filepath) == 0)
+    {
+        return EXIT_FAILURE;
+    }
+
     int totalMovies;
     struct Movie *list = processFile(filepath, &totalMovies);
-    printMovieList(list, totalMovies);
+
+    printf("Processed file %s and parsed data for %d movies\n\n", filepath, totalMovies);
+
+    int queryOption = getQueryOption();
+    printf("%d is avalid option\n", queryOption);
+
+    // printMovieList(list, totalMovies);
     return EXIT_SUCCESS;
+}
+
+char *getFilepath(int argc, char **argv)
+{
+    char *filepath = "";
+    if (argc == 2)
+    {
+        filepath = argv[1];
+    }
+    else
+    {
+        printf("Please provide path to movies CSV\n");
+        printf("e.g. ./data/movies.csv\n");
+    }
+    return filepath;
 }
 
 struct Movie *processFile(char *filepath, int *totalMovies)
@@ -139,5 +169,44 @@ void printMovieList(struct Movie *list, int totalMovies)
     {
         printMovie(list);
         list = list->next;
+    }
+}
+
+int getQueryOption(void)
+{
+    int option;
+    do
+    {
+        printOptions();
+        scanf("%d", &option);
+        printf("\n\n");
+    } while (option < 1 || option > 4);
+
+    return option;
+}
+
+void printOptions(void)
+{
+    printf("1. Show movies released in the specified year\n");
+    printf("2. Show highest rate movie for each year\n");
+    printf("3. Show the title and year of release of all movies in a specific language\n");
+    printf("4. Exit from the program\n");
+    printf("Enter a choice from 1 to 4: ");
+}
+
+void printMoviesWithYear(struct Movie *list, int year)
+{
+    int nMoviesWithYear = 0;
+    while (list != NULL)
+    {
+        if (list->year == year)
+        {
+            printf("%s\n", list->title);
+            nMoviesWithYear++;
+        }
+    }
+    if (nMoviesWithYear == 0)
+    {
+        printf("No data about movies released in year %d\n\n", year);
     }
 }
